93d52b0738aec924b1fa37ae95de073cc88c2220
Neeraj <neeraj.nathany@gmail.com>
2020-11-15 16:23:38 +0530

    PageView put back in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import Notification from './components/Notification';
-import PageView from './components/PageView';
+//import PageView from './components/PageView';
+import NewsView from './components/NewsView';
 import ArticleView from './components/ArticleView';
 import Link from './components/Link';
-import {categories,KEY} from './constants';
+import {categories,KEY,PARAMS} from './constants';
 import axios from 'axios';
 
 class App extends Component {
-    state = { selectedItem: null, sections:[] }
+    state = { selectedItem: null, sections: [], articles: [], title: '', location: window.location.pathname }
 
     componentDidMount(){
         this.getSections();
+        const onLocationChange = () => {
+            this.setState({location: window.location.pathname});
+        };
+        window.addEventListener('popstate', onLocationChange);
+        return () => {
+            window.removeEventListener('popstate', onLocationChange);
+        };
     }
 
     getSections = async () => {
@@ -26,7 +34,17 @@ class App extends Component {
             return categories.includes(e.id);
         })});
     }
-
+    
+    getNews = async () => {
+        var path = this.state.location === '/' ? '/search' : this.state.location;
+        const response = await axios.create({
+            baseURL: 'https://content.guardianapis.com',
+        }).get(path, {
+            params:PARAMS
+        });
+        this.setState({articles:response.data.response.results, title:response.data.response.section ? response.data.response.section.webTitle : 'Headlines'});
+    }
+    
     showArticle = (item) => {
         this.setState({selectedItem:item});
     }
@@ -35,29 +53,10 @@ class App extends Component {
         this.setState({selectedItem:null});
     }
     
-    getView = () => {
-        return (
-            <main className="main">
-                <aside className="panel">
-                    <div className="panel-box">
-                        <div className="panel-box-group">
-                            <h6 className="panel-box-title">Categories</h6>
-                            <Link className={"panel-button "+(window.location.pathname === '/' ? "true" : "false")} href="/">
-                                <span>All headlines</span>
-                            </Link>
-                            {this.state.sections.map(each => {
-                                return (
-                                    <Link className={"panel-button "+(window.location.pathname === '/'+each.id ? "true" : "false")} href={"/"+each.id}>
-                                        <span>{each.webTitle}</span>
-                                    </Link>
-                                )
-                            })}                                                                                 
-                        </div>
-                    </div>
-                </aside>
-                <PageView showArticle={this.showArticle}/>                
-                {this.state.selectedItem ? <ArticleView item={this.state.selectedItem} clearItem={this.clearArticle}/> : <ArticleView class="inactive"/>}
-            </main>
+    getView = () => {        
+        this.getNews();
+        return (                        
+            <NewsView key={this.state.title} showItem={this.showArticle} articles={this.state.articles} title={this.state.title}/>                                 
         )                
     }
 
@@ -68,8 +67,28 @@ class App extends Component {
                     <span className="header-title">Acronym</span>
                     <Notification  />
                     <span className="header-user">{new Date().toDateString()}</span>
-                </header>  
-                {this.getView()}
+                </header>
+                <main className="main">
+                    <aside className="panel">
+                        <div className="panel-box">
+                            <div className="panel-box-group">
+                                <h6 className="panel-box-title">Categories</h6>
+                                <Link className={"panel-button "+(this.state.location === '/' ? "true" : "false")} href="/">
+                                    <span>All headlines</span>
+                                </Link>
+                                {this.state.sections.map(each => {
+                                    return (
+                                        <Link className={"panel-button "+(this.state.location === '/'+each.id ? "true" : "false")} href={"/"+each.id}>
+                                            <span>{each.webTitle}</span>
+                                        </Link>
+                                    )
+                                })}                                                                                 
+                            </div>
+                        </div>
+                    </aside>
+                    {this.getView()}
+                    {this.state.selectedItem ? <ArticleView item={this.state.selectedItem} clearItem={this.clearArticle}/> : <ArticleView class="inactive"/>}
+                </main>
             </div>
          )
     }
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 const categories = 'business,politics,technology,sport,food,world,travel,money,culture,film,fashion';
-const KEY = '829c4e68-c5a7-43fa-8a81-ed2e567dc96a';
+//const KEY = '829c4e68-c5a7-43fa-8a81-ed2e567dc96a';
+const KEY = '9aa4cf96-f480-4cef-81b3-320fa7267fda'
 const PARAMS = {"api-key": KEY, "page-size":30, "show-fields": "body,thumbnail,wordcount"};
 
 export {categories,KEY,PARAMS};
